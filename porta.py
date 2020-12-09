fc4cc5db32d2a69c7c12a1e7c2aa87f101b78655
Lester016 <lesterdeguzman1999@gmail.com>
2020-12-09 22:35:01 +0800

    Add vigenere cipher

--- porta.py
+++ porta.py
@@ -110,6 +110,45 @@ def get_opponent(table, char):
         return table[1][col] if row == 0 else char
 
 
+# This function generates the
+# key in a cyclic manner until
+# it's length isn't equal to
+# the length of original text
+def generateKey(string, key):
+    key = list(key)
+    if len(string) == len(key):
+        return key
+    else:
+        for i in range(len(string) - len(key)):
+            key.append(key[i % len(key)])
+    return "".join(key)
+
+
+# This function returns the
+# encrypted text generated
+# with the help of the key
+def cipherText(string, key):
+    cipher_text = []
+    for i in range(len(string)):
+        x = (ord(string[i]) + ord(key[i])) % 26
+        x += ord("A")
+        cipher_text.append(chr(x))
+    return "".join(cipher_text)
+
+
+# This function decrypts the
+# encrypted text and returns
+# the original text
+def originalText(cipher_text, key):
+    orig_text = []
+    for i in range(len(cipher_text)):
+        x = (ord(cipher_text[i]) - ord(key[i]) + 26) % 26
+        x += ord("A")
+        orig_text.append(chr(x))
+    return "".join(orig_text)
+
+
+# Driver code
 if __name__ == "__main__":
     import doctest
 
@@ -124,5 +163,12 @@ if __name__ == "__main__":
     text = input("ENTER TEXT TO ENCRYPT: ").strip()
     cipher_text = encrypt(key, text)
 
-    print(f"ENCRYPTED: {cipher_text}")
-    print(f"DECRYPTED WITH KEY: {decrypt(key, cipher_text)}")
+    # print(f"ENCRYPTED: {cipher_text}")
+    # print(f"DECRYPTED WITH KEY: {decrypt(key, cipher_text)}")
+
+    string = text.upper()
+    keyword = cipher_text
+    key = generateKey(string, keyword)
+    cipher_text = cipherText(string, key)
+    print("ENCRYPTED:", cipher_text)
+    print("DECRYPTED WITH KEY:", originalText(cipher_text, key))
